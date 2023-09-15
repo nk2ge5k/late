01a36e61af7dc6070ba5c04cf7d0df6d616044d8
Max Udalov <re.udalov@gmail.com>
2023-09-15 20:38:11 +0400

    tests: update keyset test

--- tests/test_keyset.py
+++ tests/test_keyset.py
@@ -1,8 +1,7 @@
 from grpclib.client import Channel
 from testsuite.utils import matching
 
-from api.proto.v1 import admin_pb2
-from api.proto.v1 import keyset_pb2
+from api.proto.v1 import admin_pb2, keyset_pb2
 
 
 async def test_create_get(project_api, keyset_api, message_to_dict):
@@ -25,10 +24,24 @@ async def test_create_get(project_api, keyset_api, message_to_dict):
         )
     )
 
-    assert response == {
-        "keyset": {
-            "id": matching.any_string,
-            "project_id": project_id,
-            "name": NAME,
-        }
+    assert "keyset" in response
+    keyset = response["keyset"]
+
+    assert keyset == {
+        "id": matching.any_string,
+        "project_id": project_id,
+        "name": NAME,
     }
+
+    response = message_to_dict(
+        await keyset_api.GetKeysets(
+            keyset_pb2.GetKeysetsRequest(
+                project_id=project_id,
+            )
+        )
+    )
+    assert "keysets" in response
+
+    keysets = response["keysets"]
+    assert len(keysets) == 1
+    assert keysets.pop() == keyset
